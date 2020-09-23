8b5d50ab40b05c68af9aab20f5f8b60c25fb83ba
Gustavo Carrillo <g2c100@hotmail.com>
2020-09-22 19:24:35 -0700

    Created a new Project Called GenericCollections in which all generic types are demostrated. (List, Linkedlist, hash, etc)

--- GenericCollections/Program.cs
+++ GenericCollections/Program.cs
@@ -9,128 +9,147 @@ namespace GenericCollections
         {
 
 
-            Console.WriteLine("With List<T>:");
-            foreach (var item in Lister())
+            static List<int> GetMyList()
+            {
+                List<int> mylist = new List<int>(5);
+
+                foreach (int integers in new int[] { 5, 6, 7, 8, 9 })
+                {
+                    mylist.Add(integers);
+                }
+
+                return mylist;
+            }
+
+            Console.WriteLine("Using a List<T>:");
+            foreach (var item in GetMyList())
             {
                 Console.WriteLine(item);
             }
+
+
+
+            static LinkedList<int> GetMyLinkedList()
+            {
+                LinkedList<int> numbers = new LinkedList<int>();
+
+                foreach (int number in new int[] { 23, 84, 47, 44, 6 })
+                {
+                    numbers.AddFirst(number);
+                }
+                return numbers;
+            }
+
             Console.WriteLine();
-            Console.WriteLine("With LinkedList:");
-            foreach (var item in Link())
+            Console.WriteLine("Using a LinkedList:");
+            foreach (var item in GetMyLinkedList())
             {
                 Console.WriteLine(item);
             }
+
+
+
+            static Queue<int> GetMyQue()
+            {
+                Queue<int> numbers = new Queue<int>();
+
+                foreach (int number in new int[] { 1, 2, 3, 4, 5 })
+                {
+                    numbers.Enqueue(number);
+                }
+                return numbers;
+
+            }
+
             Console.WriteLine();
-            Console.WriteLine("With Queue:");
-            foreach (var item in Que())
+            Console.WriteLine("Using a Queue:");
+            foreach (var item in GetMyQue())
             {
                 Console.WriteLine(item);
             }
+
+
+
+
+
+            static Stack<int> GetMyStack()
+            {
+                Stack<int> numbers = new Stack<int>();
+                foreach (int number in new int[] { 1, 42, 3, 12, 85 })
+                {
+                    numbers.Push(number);
+                }
+                return numbers;
+            }
+
             Console.WriteLine();
-            Console.WriteLine("With Stack:");
-            foreach (var item in Stacker())
+            Console.WriteLine("Using a Stack:");
+            foreach (var item in GetMyStack())
             {
                 Console.WriteLine(item);
             }
+
+
+
+
+
+            static Dictionary<string, string> GetMyDictionary()
+            {
+                Dictionary<string, string> mssa = new Dictionary<string, string>();
+
+                mssa.Add("Dan", "Teacher");
+                mssa.Add("Gus", "Student");
+                mssa.Add("Carolyn", "Boss Lady");
+                mssa.Add("Tyron", "Cool guy");
+
+                return mssa;
+            }
+
             Console.WriteLine();
-            Console.WriteLine("With Dictionary:");
-            foreach (var item in Dict())
+            Console.WriteLine("Using a Dictionary:");
+            foreach (var item in GetMyDictionary())
             {
                 Console.WriteLine(item);
             }
+
+
+
+
+            static SortedList<string, int> GetMySortedCars()
+            {
+                SortedList<string, int> cars = new SortedList<string, int>();
+
+                cars.Add("Camaro", 1);
+                cars.Add("Mustang", 2);
+                cars.Add("Charger", 3);
+                cars.Add("Pinto", 4);
+
+                return cars;
+            }
+
             Console.WriteLine();
-            Console.WriteLine("With SortedList:");
-            foreach (var item in Sorter())
+            Console.WriteLine("Using a Sorted List:");
+            foreach (var item in GetMySortedCars())
             {
                 Console.WriteLine(item);
             }
+
+
+
+            static HashSet<int> GetMyHashTable()
+            {
+                HashSet<int> numbers = new HashSet<int>(new int[] { 45, 28, 81, 14, 52 });
+
+                return numbers;
+            }
+
             Console.WriteLine();
             Console.WriteLine("With HashSet:");
-            foreach (var item in Hash())
+            foreach (var item in GetMyHashTable())
             {
                 Console.WriteLine(item);
             }
-
-
         }
-
-        //List<T>
-        static List<int> Lister()
-        {
-            List<int> list = new List<int>(5);
-
-            foreach (int number in new int[] { 11, 22, 33, 44, 55 })
-            {
-                list.Add(number);
-            }
-
-            return list;
-        }
-        public static LinkedList<int> Link()
-        {
-            LinkedList<int> numbers = new LinkedList<int>();
-
-            foreach (int number in new int[] { 11, 22, 33, 44, 55 })
-            {
-                numbers.AddFirst(number);
-            }
-            return numbers;
-        }
-        public static Queue<int> Que()
-        {
-            Queue<int> numbers = new Queue<int>();
-
-            foreach (int number in new int[] { 11, 22, 33, 44, 55 })
-            {
-                numbers.Enqueue(number);
-            }
-            return numbers;
-
-        }
-        public static Stack<int> Stacker()
-        {
-            Stack<int> numbers = new Stack<int>();
-            foreach (int number in new int[] { 11, 22, 33, 44, 55 })
-            {
-                numbers.Push(number);
-            }
-            return numbers;
-        }
-        public static Dictionary<string, int> Dict()
-        {
-            Dictionary<string, int> playoff = new Dictionary<string, int>();
-
-            playoff.Add("Alabama", 1);
-            playoff.Add("Clemson", 2);
-            playoff.Add("Notre Dame", 3);
-            playoff.Add("LSU", 4);
-
-            return playoff;
-        }
-        public static SortedList<string, int> Sorter()
-        {
-            SortedList<string, int> playoff = new SortedList<string, int>();
-
-            playoff.Add("Alabama", 1);
-            playoff.Add("Clemson", 2);
-            playoff.Add("Notre Dame", 3);
-            playoff.Add("LSU", 4);
-
-            return playoff;
-        }
-        public static HashSet<int> Hash()
-        {
-            HashSet<int> numbers = new HashSet<int>(new int[] { 11, 22, 33, 44, 55 });
-
-            return numbers;
-        }
-
-
-
-
-
-    }
-
     }
 }
-   
+
